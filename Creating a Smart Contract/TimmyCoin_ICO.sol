5321973f59ec7faba54df8e394a2a8423c24e369
Gurbaaz Singh Gill <gurbaaz19@gmail.com>
2022-07-04 04:53:34 +0530

    Smart Contract

--- Creating a Smart Contract/TimmyCoin_ICO.sol
+++
@@ -1,7 +0,0 @@
-pragma solidity ^0.8.0;
-
-contract TimmyCoin_ICO {
-    function TimmyCoin_ICO(){
-
-    }
-}
---
+++ Creating a Smart Contract/timmycoin_ico.sol
@@ -0,0 +1,16 @@
+// TimmyCoin ICO
+
+// Compiler Version
+pragma solidity ^0.8.0;
+
+contract timmycoin_ico {
+    // Introducing total number of TimmyCoins to be circulated
+    uint public max_timmycoins = 1000000;
+
+    // Introducing the USD - TimmyCoin conversion rate
+    uint public used_to_timmycoins = 1000;
+
+    // Introducing the Timmy Coins bought by investors
+
+    uint public total_timmycoins_bought = 0;
+}
